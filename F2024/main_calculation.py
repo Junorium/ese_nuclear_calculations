5bcc27c83dccc270c52f9b3732424c6109e5bb51
Jr Perez <jrp527@lehigh.edu>
2024-09-24 19:17:32 -0400

    init project code

---
+++ F2024/main_calculation.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import math
+
+mesh_n = input() # insert mesh number
+
+chi_val = [] # insert chi values
+
+def reactor(diameter, height, reflector_diameter, reflector_height):
