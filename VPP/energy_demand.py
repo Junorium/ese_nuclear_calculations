5b755ce26ea937a5a8674e55d1c0304b6ae8a7b1
Junorium <johnrichard.perez.2004@gmail.com>
2025-02-20 20:44:22 -0500

    create a min/max functionality with graphing

--- VPP/energy_demand.py
+++ VPP/energy_demand.py
@@ -2,7 +2,25 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-df = pd.read_csv('CAISO_demand.csv', header=None)
-print(df.head)
-print("-----")
-print(df.columns)
+df = pd.read_csv('CAISO-demand-20250220.csv', index_col=0)
+
+demand = df.iloc[2]
+min_demand_time = demand.idxmin() # TIME INDEX, not min demand value
+max_demand_time = demand.idxmax() # TIME INDEX, not max demand value
+
+print(f"Minimum demand time: {min_demand_time}")
+print(f"Maximum demand time: {max_demand_time}")
+
+plt.plot(demand)
+
+# Min, Max time indications (nodes)
+plt.scatter(min_demand_time, demand.min(), color='r', zorder=5)
+plt.scatter(max_demand_time, demand.max(), color='g', zorder=5)
+
+plt.xticks(demand.index[::60])
+
+plt.xlabel('Time (min)')
+plt.ylabel('Energy demand')
+plt.title("Energy Demand for 2/20/2025")
+
+plt.show()
